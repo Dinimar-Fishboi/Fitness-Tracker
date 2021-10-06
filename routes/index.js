395c9674ae748503075cce930f1faac39936a992
Dinimar Fish <tayla@tallisman.com.au>
2021-10-06 21:14:57 +1000

    confirmed models working, added pseudocode and updated pathway

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,12 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
+// User needs to be able to ADD excercises to the most recent workout plan
+
+// User needs to be able to add NEW exercises to the workout plan
+
+// REview most recent 7 days of excercise
+
+// For this 7 day review, we are looking for the total combined WEIGHT
+
+// For this 7 day review, we are looking for the total combined DURATION
